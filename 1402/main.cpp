672270e5f0739d62f1f5d9dfa31a02316be42e57
Temp <temp@temp.temp>
2020-06-17 16:00:40 +0900

    solved 1402

---
+++ 1402/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int cnt;
+    std::cin >> cnt;
+    for (int i = 0; i < cnt; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+    }
+    for (int i = 0; i < cnt; i++)
+    {
+        std::cout << "yes\n";
+    }
+    return 0;
+}
